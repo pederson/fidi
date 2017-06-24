75c2f3323ce22ba79add0cabdd8efd16f583c28b
pederson <dpederson@utexas.edu>
2017-06-23 19:17:51 -0500

    (refactor) of ObjectStencil to have neighbors as other ObjectStencil types

--- include/ObjectStencil.hpp
+++ include/ObjectStencil.hpp
@@ -10,50 +10,55 @@ template<std::size_t dim,
 		 std::size_t reach,
 		 class ValueT>
 struct ObjectStencil : public ValueT{
+	typedef ObjectStencil<dim, reach, ValueT>			NeighborType;
+
 	// ValueT										center;
-	std::array<std::array<const ValueT *,reach>, dim>	neighbMin;
-	std::array<std::array<const ValueT *,reach>, dim>	neighbMax;		
+	std::array<std::array<const NeighborType *,reach>, dim>	neighbMin;
+	std::array<std::array<const NeighborType *,reach>, dim>	neighbMax;		
 
 	ObjectStencil() : ValueT(){};
+
+	ObjectStencil(const ObjectStencil & o) = default;
 
 	ObjectStencil(const ValueT & c) : ValueT(c){};
 
 	ObjectStencil & operator=(const ValueT & c){
 		ObjectStencil s(c);
-		std::swap(s.neighbMin, neighbMin);
-		std::swap(s.neighbMax, neighbMax);
+		// std::swap(s.neighbMin, neighbMin);
+		// std::swap(s.neighbMax, neighbMax);
+		std::swap(static_cast<ValueT &>(*this), static_cast<ValueT &>(s));
 		return *this;
 	}
 
 	constexpr ObjectStencil(const ValueT & c, 
-			const std::array<std::array<const ValueT *,reach>, dim> & nMin,
-			const std::array<std::array<const ValueT *,reach>, dim> & nMax)
+			const std::array<std::array<const NeighborType *,reach>, dim> & nMin,
+			const std::array<std::array<const NeighborType *,reach>, dim> & nMax)
 	: ValueT(c), neighbMin(nMin), neighbMax(nMax) {};
 
 	// runtime set and get
-	constexpr const ValueT & getNeighborMin(std::size_t d, std::size_t r=reach-1) const{
+	constexpr const NeighborType & getNeighborMin(std::size_t d, std::size_t r=reach-1) const{
 		return *neighbMin[d][r];
 	};
-	constexpr const ValueT & getNeighborMax(std::size_t d, std::size_t r=reach-1) const{
+	constexpr const NeighborType & getNeighborMax(std::size_t d, std::size_t r=reach-1) const{
 		return *neighbMax[d][r];
 	};
-	void setNeighborMin(const ValueT & val, std::size_t d, std::size_t r=reach-1){
+	void setNeighborMin(const NeighborType & val, std::size_t d, std::size_t r=reach-1){
 		neighbMin[d][r] = &val;
 	};
-	void setNeighborMax(const ValueT & val, std::size_t d, std::size_t r=reach-1){
+	void setNeighborMax(const NeighborType & val, std::size_t d, std::size_t r=reach-1){
 		neighbMax[d][r] = &val;
 	};
 
 
 	// compile-time set and get
 	template <std::size_t d, std::size_t r=reach-1>
-	constexpr const ValueT & getNeighborMin() const {return *std::get<r>(std::get<d>(neighbMin));};
+	constexpr const NeighborType & getNeighborMin() const {return *std::get<r>(std::get<d>(neighbMin));};
 	template <std::size_t d, std::size_t r=reach-1>
-	constexpr const ValueT & getNeighborMax() const {return *std::get<r>(std::get<d>(neighbMax));};
+	constexpr const NeighborType & getNeighborMax() const {return *std::get<r>(std::get<d>(neighbMax));};
 	template <std::size_t d, std::size_t r=reach-1>
-	constexpr void setNeighborMin(const ValueT & val){std::get<r>(std::get<d>(neighbMin)) = &val;};
+	constexpr void setNeighborMin(const NeighborType & val){std::get<r>(std::get<d>(neighbMin)) = &val;};
 	template <std::size_t d, std::size_t r=reach-1>
-	constexpr void setNeighborMax(const ValueT & val){std::get<r>(std::get<d>(neighbMax)) = &val;};
+	constexpr void setNeighborMax(const NeighborType & val){std::get<r>(std::get<d>(neighbMax)) = &val;};
 };
 
 
